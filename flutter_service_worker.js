eaede0b23cc3ddd9a5dfe90a479b88fbb301a706
Anirudh Vyas <ricky.nj@gmail.com>
2026-02-02 21:45:15 -0800

    Request Demo emails both founders

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8d0de2ae178ac8bc7e0b3b5ed697876f",
+const RESOURCES = {"flutter_bootstrap.js": "dc653c19df1e7e5c763f5e223bf221c1",
 "version.json": "f7af1f5d662def3462dfd937db80ed51",
-"index.html": "18169d89d0e9c6bd47d038e7ac990f30",
-"/": "18169d89d0e9c6bd47d038e7ac990f30",
+"index.html": "542ce53384616236fe11ef4dfc74f323",
+"/": "542ce53384616236fe11ef4dfc74f323",
 "main.dart.js": "0f6cdeac26b15ba3038072779d52bf07",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "266b049d19c7b3ffbb5f953f2de75bbe",
